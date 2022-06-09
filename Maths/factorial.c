1cb2dbf5cf20802023d848caca6f8900a216ffb1
Amarjit Singh <amarjitlife@gmail.com>
2022-06-09 15:20:33 +0530

    Ticket 111 : Extended Functionality Of Following Features 	Added factorial Of long Type 	Added sum Of 3 Numbers 	modified:   Maths/factorial.c 	modified:   Maths/sum.c

--- Maths/factorial.c
+++ Maths/factorial.c
@@ -2,8 +2,17 @@
 #include <stdio.h>
 
 int factorial(int n);
+long factorialLong(long n);
 
 int factorial(int n) {
+	if ( n == 1 || n == 0 ) {
+		return 1;
+	} else {
+		return n * factorial( n - 1 ) ;
+	}
+}
+
+long factorialLong(long n) {
 	if ( n == 1 || n == 0 ) {
 		return 1;
 	} else {
--- Maths/sum.c
+++ Maths/sum.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 
 int sum(int x, int y);
+int sum3(int x, int y, int z);
 
 int sum(int x, int y) {
 	return x + y;
+}
+
+int sum3(int x, int y, int z) {
+	return x + y + z;
 }
 
